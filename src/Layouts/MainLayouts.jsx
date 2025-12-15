7f7bf2219d37c2ac943dc8da76eda48384e77a61
nayem181 <nayemimranhasan@gmail.com>
2025-12-15 09:45:22 +0600

    creating logo and use it navbar with react router

---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../assets/Components/Navbar';
+
+const MainLayouts = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayouts;
